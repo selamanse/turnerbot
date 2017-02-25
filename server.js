efbf6a0df759bf691d10ef347278393523d955f8
Selamanse <selamanse@scheinfrei.info>
2017-02-25 14:17:57 +0100

    Fix input behavior

--- server.js
+++ server.js
@@ -10,24 +10,31 @@ const app = new Telegraf(process.env.BOT_TOKEN)
 
 app.command('start', (ctx) => {
   console.log('start', ctx.from)
-  ctx.reply('Aye!')
+  ctx.reply('Aye! Just give me something to turn')
 })
 
-app.on('text', (ctx) => {
-  console.log(ctx.message)
-  var promise = Turner.validate(ctx.message.text)
+const turnerMiddleware = (ctx, next) => {
+  if (!ctx.turnerInput) {
+    next()
+  }
+  console.log("RECIEVED TURNER INPUT: '" + ctx.turnerInput + "'")
 
-  promise.then(function (res) {
+  Turner.validate(ctx.turnerInput)
+  .then(function (res) {
     if (res === true) {
-      ctx.reply('Aye! that be right!')
+      ctx.reply('Aye! that be right @' + ctx.from.username + '! <b>' + ctx.turnerInput + '</b> is valid', {parse_mode: 'Html'})
     } else {
-      ctx.reply("Walk th' plank ye scallywag!")
+      ctx.reply('Walk th\' plank ye scallywag @' + ctx.from.username + '! <b>' + ctx.turnerInput + '</b> is not valid', {parse_mode: 'Html'})
     }
   }).catch(function (rej) {
     ctx.reply('Arr! some error happened.')
   })
-})
+}
 
-app.on('sticker', (ctx) => ctx.reply('Arr!'))
+// Turn RegEx
+app.hears(/turn (.*)/, (ctx, next) => {
+  ctx.turnerInput = ctx.match[1]
+  next()
+}, turnerMiddleware)
 
 app.startPolling()
